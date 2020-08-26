60b960bf5bdd56691489b9dc65651391d9edd8e7
root <root@images.os.selectel.org>
2020-08-26 19:58:36 +0000

    add regUser

--- app/routes/databaseRoutes.js
+++ app/routes/databaseRoutes.js
@@ -1,103 +1,54 @@
-const {JsonDB} = require('node-json-db');
-const {Config} = require('node-json-db/dist/lib/JsonDBConfig');
-const library = require('../routes/library');
-const nodemailer = require('nodemailer');
-const needle = require('needle');
-const { ObjectID } = require('bson');
-const { ResumeToken } = require('mongodb');
-const { count } = require('console');
-
-
-
-let transporter = nodemailer.createTransport({ //Создание почтового бота
-    service: 'gmail',
-    auth: {
-        user: 'noreplyprofevents@gmail.com',
-        pass: 'QETUOADGJLZCBM13579*'
-    }
-})
+const crypto = require('crypto');
 
 
 module.exports = function(app, client) {
-    app.post('/api/alertEmail',function(req,res){ //Рассылка ошибок
-        if (req.body.err == "310"){
-            transporter.sendMail({
-                from: '"no-reply_ProfEvents" <noreplyprofevents@gmail.com>',
-                to: req.body.email,
-                subject: "Предупреждение о подозрительной активности",
-                text: "Кто то пытался зайти в Ваш аккаунт. Если это не вы, то нажмите кнопку ниже",
-            })
-        }
+    app.post('/api/regUser',function(req,res){ //Получение данных с регистрации +
+        email = req.body.email;
+        sessionID = req.body.sessionid;
+        user = req.body.user;
 
-    });
-
-    app.post('/api/registration',function(req,res){ //Получение данных с регистрации +
         const mdb = client.db("userData");
-        var collection = mdb.collection("information");
+        var collection = mdb.collection("users");
+        var collectionI = mdb.collection("sessionID");
         
-        collection.findOne({email: req.body.email},function(err, result){
-            if (err) throw err;
-
-            if (result == null){
-                const mdb = client.db("userData");
-                var collection = mdb.collection("information");
-        
-                collection.find().toArray(function(err,data){
+        collectionI.findOne({email: email}, function(errI, sesData){
+            if (errI) throw errI
+            
+            if (sesData.sessionID == sessionID){
+                collection.findOne({email: req.body.email},function(err, result){
                     if (err) throw err;
-                    
-                    userId = String(data.length); //Задание всех базовых параметров человека
 
-                    user = req.body;
-                    user.friends = [];
-                    user.statNumber = "Номер еще не создан";
-                    user.role = "user";
-                    user.tester = false;
-                    user.userId = userId;
-    
-                    key = library.keyGen(); //Генерация начального sessionID
-                    var collection = mdb.collection("sessionID");
-                    collection.insertOne({email: req.body.email, sessionID: key}, function(err){
-                        if (err) throw err;
-                    })
-    
-                    var collection = mdb.collection("information");
-                    collection.insertOne(user, function(err){ //Сохранение информации
-                        if (err) throw (err);
-                    });
-    
-                    var collection = mdb.collection("checkedEvents"); //Задание начальных параметров списка мероприятий
-                    collection.insertOne({checkedEvents: [],email: req.body.email, stat: {service: 0, programming: 0, engineering: 0}, userId: userId, confStat: {service: 0, programming: 0, engineering: 0}}, function(err){
-                        if (err) throw (err);
-                    });
-                    
-                    var collection = mdb.collection("avatars");
-                    collection.findOne({token: "1"}, function(err,result){
-                        if (err) throw err;
-
-                        collection.insertOne({email: req.body.email, data: {file: result.baseAvatar, type: "image/jpeg"}}, function(err){
+                    if (result == null){
+                        key = crypto.randomBytes(64).toString('hex');
+                        var collection = mdb.collection("sessionID");
+                        collection.insertOne({email: req.body.email, sessionID: key}, function(err){
                             if (err) throw err;
+                        })
+            
+                        var collection = mdb.collection("users");
+                        collection.insertOne(req.body.user, function(err){ //Сохранение информации
+                            if (err) throw (err);
                         });
-                    });
-
-    
-                    var collection = mdb.collection("portfolios");
-                    collection.insertOne({email: req.body.email, data: []}, function(err){
-                        if (err) throw err;
-                    });
-
-                    res.send('Reg succsesful');
+            
+                        res.send('Reg succsesful');
+                    }
+                    else{
+                        res.send('Reg Fail');
+                    }
                 });
             }
             else{
-                res.send('Reg Fail');
+                res.send("310");
             }
         });
     });
-
+    
     app.post('/api/login',function(req,res){ //Получение данных с логина и отправка обратно SessionID +    
         const mdb = client.db("userData");
-        var collection = mdb.collection("information");
+        var collection = mdb.collection("users");
 
+        console.log(req.body)
+        
         collection.findOne({email: req.body.email}, function(err, data){
             if (err) throw err;
 
@@ -122,24 +73,61 @@ module.exports = function(app, client) {
         });
     });
 
-    app.post('/api/adminLogin',function(req,res){ //Получение данных с логина и отправка обратно SessionID +
+    app.post('/api/getInformation',function(req,res){  //Получение информации о пользователе +
+        email = req.body.email;
+        sessionID = req.body.sessionid;
+
+        console.log(req.body)
+
         const mdb = client.db("userData");
-        var collection = mdb.collection("information");
+        var collection = mdb.collection("users");
+        var collectionI = mdb.collection("sessionID");
 
-        collection.findOne({email: req.headers.email}, function(err, data){
-            if (err) throw err;
-
-            if (data != null){
-                if (data.password == req.headers.password){
-                    res.json(data.role)
-                }
-                else{
-                    res.json('Incorect password');
-                }
+        collectionI.findOne({email: email}, function(errI, sesData){
+            if (errI) throw errI
+            
+            if (sesData.sessionID == sessionID){
+                collection.findOne({email: email}, function(err, inform){
+                    if (err) throw err;
+        
+                    res.json(inform);
+                });
             }
             else{
-                res.json('User undefined')
-            }    
+                res.send("310");
+            }
+        });  
+    });
+
+    app.post("/api/getUserList",function(req,res){
+        email = req.body.email;
+        sessionID = req.body.sessionid;
+
+        const mdb = client.db("userData");
+        var collection = mdb.collection("users");
+        var collectionI = mdb.collection("sessionID");
+
+        collectionI.findOne({email: email}, function(errI, sesData){
+            if (errI) throw errI
+            
+            if (sesData.sessionID == sessionID){
+                collection.find().toArray(function(err, list){ 
+                    if (err) throw err;
+                    
+                    resList = []
+
+                    for (i in list){
+                        if (list[i].role != admin){
+                            resList.push({email: list[i].email,role: list[i].role})
+                        }
+                    }
+
+                    res.send(resList)
+                });
+            }
+            else{
+                res.send("310");
+            }
         });
     });
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -20,27 +20,12 @@ const multer  = require("multer");
 //const url = 'mongodb://localhost:27017';
 const port = 3000; //Основной порт сервера
 
- 
-var confCodeJSONdb = new JsonDB(new Config("confirmCode", true, false, '/')); //БД ключей залогиненых пользователей
-
 
 MongoClient.connect('mongodb://127.0.0.1:27017/' , (err, client) => { //Подключение к базе данных и загрузка ручек для обращения к ней
     if (err) throw err;
     console.log("База данных успешно подключена");
     require('../routes/databaseRoutes')(app, client); //Подключение всех ручек из databaseRoutes.js
 });
-
-
-
-
-let transporter = nodemailer.createTransport({ //Создание почтового бота
-    service: 'gmail',
-    auth: {
-        user: 'noreplyprofevents@gmail.com',
-        pass: 'QETUOADGJLZCBM13579*'
-    }
-})
-console.log("Почтовый бот подключен")
 
 
 app.use (bodyParser.json ({limit: '50mb', extended: true})) //Запуск библиотек сервером
@@ -54,44 +39,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(expressMongoDb('mongodb://127.0.0.1:27017/'));
-
-
-
-
-app.post('/api/mailCheck',function(req,res){ //Отправка на почту кода с подтверждением
-    email = req.body.email;
-    code = String(Math.round(Math.random()*10)) + String(Math.round(Math.random()*10)) + String(Math.round(Math.random()*10)) + String(Math.round(Math.random()*10)) + String(Math.round(Math.random()*10)) + String(Math.round(Math.random()*10)); 
-    confCodeJSONdb.push('/'+email, code)
-    transporter.sendMail({
-        from: '"no-reply_ProfEvents" <noreplyprofevents@gmail.com>',
-        to: email,
-        subject: "Код подтверждения",
-        text: "Ваш код подтверждения: " + String(code) + "\n\n\n\n\nЕсли это не Вы, то просто проигнорируйте данное письмо",
-    })
-});
-
-app.post('/api/codeCheck',function(req,res){ //Проверка кода подтверждения
-    email = req.body.email;
-    code = req.body.code;
-    rCode = confCodeJSONdb.getData('/'+email);
-
-    console.log(email,code,rCode)
-
-
-    if (code == rCode){
-        console.log("OK")
-        res.send("OK")
-    }
-    else{
-        res.send("Incorect code")
-    }
-});
-
-
-
-
-
-
 
 
 app.listen(port, "0.0.0.0", () => {
