ed5a3cfa860438889256ce964693c483989dcd15
MakualDev <31779213+max2000505@users.noreply.github.com>
2021-01-13 00:08:08 +0300

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,3 @@
-//var http = require('http');
-//var needle = require('needle');
-//var fs = require('fs');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -11,13 +8,9 @@ const {Config} = require('node-json-db/dist/lib/JsonDBConfig');
 const cors = require('cors');
 const fetch = require('node-fetch');
 const nodemailer = require('nodemailer');
-//const crypto = require('crypto');
 const MongoClient = require('mongodb').MongoClient;
 const expressMongoDb = require('express-mongo-db');
 const multer  = require("multer");
-//const library = require('../routes/library');
-//const assert = require('assert');
-//const url = 'mongodb://localhost:27017';
 const port = 3000; //Основной порт сервера
 
 
@@ -37,8 +30,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
-
-// app.use(expressMongoDb('mongodb+srv://makual:8eGs8my&$zj@cluster0.7b2em.mongodb.net/userData?retryWrites=true&w=majority'));
 
 
 app.listen(port, "0.0.0.0", () => {
